339d1825d998620900a7f7483e334d0fbe7abfad
matthew.serre <matthew.serre@gmail.com>
2018-10-28 23:06:45 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,5 +18,8 @@ def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   if array.include?("cheddar" || "gouda" || "camembert") == TRUE
-    array.detect?{|cheese| cheese
+    array.detect?{|cheese| cheese == "cheddar" || "gouda" || "camembert"}
+  else
+    NIL
+  end
 end
