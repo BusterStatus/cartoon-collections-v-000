63ca7df5cf81e70b7a29bb55cf6fa3338436e7ea
matthew.serre <matthew.serre@gmail.com>
2018-10-28 22:46:17 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@
 
 def summon_captain_planet(array)
  calls = []
- array.collect { |call| call.upcase + "!"
+ array.collect { |call| "#{call.upcase}!" }
 end
 
 def long_planeteer_calls# code an argument here
